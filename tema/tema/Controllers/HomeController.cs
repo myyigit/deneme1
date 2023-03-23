5cd4b813809ac6bd8cd037c8a35411a6fec024c2
muhammet yavuz <myyigit@gmail.com>
2023-03-23 21:35:34 +0300

    deneme1
    boostrap deneme tema

---
+++ tema/tema/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tema.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Contact() 
+        {
+            return View();
+        }
+
+        public ActionResult Portfolio() 
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Team()
+        {
+            return View();
+        }
+
+        public ActionResult PortfolioDetails()
+        {
+            return View();
+        }
+
+        public ActionResult Testimonials()
+        {
+            return View();
+        }
+    }
+}
